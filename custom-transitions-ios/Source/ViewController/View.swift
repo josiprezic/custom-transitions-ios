66054b117b3a49df08cbb0eba611391ea18a008c
josiprezic <josip@feelmorelabs.com>
2020-03-13 12:24:12 +0100

    Add table view

--- custom-transitions-ios/Source/ViewController/View.swift
+++ custom-transitions-ios/Source/ViewController/View.swift
@@ -6,16 +6,47 @@
 //  Copyright © 2020 Josip Rezic. All rights reserved.
 //
 
-import UIKit
+import SnapKit
 
 final class View: UIView {
     
+    //
+    // MARK: - Properties
+    //
+    
+    let tableView = UITableView()
+    
+    //
+    // MARK: - Initializers
+    //
+    
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
-        backgroundColor = .red
+        addSubviews()
+        setupStyling()
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    //
+    // MARK: - Private methods
+    //
+    
+    private func addSubviews() {
+        addSubview(tableView)
+    }
+    
+    private func setupStyling() {
+        backgroundColor = .gray
+        tableView.backgroundColor = .clear
+    }
+    
+    private func setupConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
--- custom-transitions-ios/Source/ViewController/ViewController.swift
+++ custom-transitions-ios/Source/ViewController/ViewController.swift
@@ -8,17 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Transition: CaseIterable {
+    case test
+}
+
+final class ViewController: UIViewController {
     
-    let contentView = View()
+    //
+    // MARK: - Properties
+    //
+    
+    private let contentView = View()
+    private let transitions = Transition.allCases
+    
+    //
+    // MARK: - View methods
+    //
     
     override func loadView() {
         view = contentView
+        setupTableView()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         debugPrint("View did load")
+    }
+    
+    //
+    // MARK: - Private methods
+    //
+    
+    private func setupTableView() {
+        contentView.tableView.delegate = self
+        contentView.tableView.dataSource = self
+        contentView.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "id")
+    }
+    
+    private func handleNewTransition(transition: Transition) {
+        
+    }
+}
+
+//
+// MARK: - Extension - UITableViewDelegate
+//
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let transition = transitions[indexPath.row]
+        handleNewTransition(transition: transition)
+    }
+}
+
+//
+// MARK: - Extension - UITableViewDelegate
+//
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Transition.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "id") else {
+            return UITableViewCell()
+        }
+        
+        cell.textLabel?.text = "\(transitions[indexPath.row])"
+        cell.backgroundColor = .clear
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
     }
 }
 
